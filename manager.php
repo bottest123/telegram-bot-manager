2bc7e2c5a9f5a3320db27ced2088cc855d5e934f
bottest123 <41206183+bottest123@users.noreply.github.com>
2018-07-31 15:35:03 +0530

    Update manager.php

--- manager.php
+++ manager.php
@@ -8,7 +8,7 @@ try {
         'api_key'      => '620472523:AAF-6nir55yctdZ8P3n_A3YZp_b5Q0I3Uc8',
         // Extras.
         'bot_username' => 'testitbot',
-        'secret'       => 'amitpandey123121',
+        'secret'       => '91268D98ECA2DA44ECFCFFC2D3571FCC35FB00ED783D792F32BC80B7A0632E03',
         'webhook'      => [
             'url' => 'https://freelikes4insta.000webhostapp.com/bot/2/manager.php',
         ]
